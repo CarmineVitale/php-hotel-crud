0e89ee2ed0147e2ebf9eb89b4575399dbfc23c99
CarmineVitale <carmine.vitale90@gmail.com>
2020-05-29 13:02:42 +0200

    Aggiunta Navbar

--- index.php
+++ index.php
@@ -3,43 +3,43 @@ include __DIR__ . '/partials/home/server.php';
 include __DIR__ . '/partials/templates/head.php'
 ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <header>
-                    <h1>Hotel California</h1>
-                </header>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <header>
+                <h1>Hotel California</h1>
+            </header>
 
-                <table class="table">
-                    <thead>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Codice</th>
+                        <th>Numero</th>
+                        <th>Piano</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    if (!empty($rooms)) { 
+                        foreach ($rooms as $room) { ?>
                         <tr>
-                            <th>Codice</th>
-                            <th>Numero</th>
-                            <th>Piano</th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                        if (!empty($rooms)) { 
-                            foreach ($rooms as $room) { ?>
-                              <tr>
-                                  <td><?php echo $room['id']; ?></td>
-                                  <td><?php echo $room['room_number']; ?></td>
-                                  <td><?php echo $room['floor']; ?></td>
-                                  <td><a href="./show.php?id=<?php echo $room['id']; ?>"> Visualizza</a></td>
-                                  <td>Modifica</td>
-                                  <td>Cancella</td>
-                              </tr>  
-                        <?php }
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
+                            <td><?php echo $room['id']; ?></td>
+                            <td><?php echo $room['room_number']; ?></td>
+                            <td><?php echo $room['floor']; ?></td>
+                            <td><a href="./show.php?id=<?php echo $room['id']; ?>"> Visualizza</a></td>
+                            <td>Modifica</td>
+                            <td>Cancella</td>
+                        </tr>  
+                    <?php }
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
-    <?php include __DIR__ . '/partials/templates/footer.php' ?>
+</div>
+<?php include __DIR__ . '/partials/templates/footer.php' ?>
     
--- partials/templates/head.php
+++ partials/templates/head.php
@@ -7,3 +7,13 @@
     <link rel="stylesheet" href="dist/css/app.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Hotel California</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Torna a 'ALL ROOMS' <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
